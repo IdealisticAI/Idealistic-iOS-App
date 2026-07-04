26ac1ca4e6f4bec8cf9f6c78bd25badf3d3cb503
Evangelos <vaggdedes@gmail.com>
2026-07-04 17:34:06 +0300

    Initial Commit

---
+++ Idealistic/IdealisticApp.swift
@@ -0,0 +1,17 @@
+//
+//  IdealisticApp.swift
+//  Idealistic
+//
+//  Created by Evangelos Dedes on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct IdealisticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
